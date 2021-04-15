54fccb1e431f566a07fa4c0a45fe1d28797c9ec1
manishaj <manisha.jain@tesco.com>
2021-04-15 19:45:18 +0530

    do not run in packer

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,8 +10,9 @@ git clone https://github.com/evermanisha/spring-petclinic-rest
 
 cd spring-petclinic-rest/
 mvn install
-cd target
-java -jar spring-petclinic-rest-2.4.2.jar
+#cd target
+#java -jar spring-petclinic-rest-2.4.2.jar
+
 
 
 #cd spring-petclinic-rest/
